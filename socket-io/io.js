abd084442eff1ebcdd752098314def678b95feec
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-08-15 16:44:55 -0300

    addin an option to let the technician know that a there are updates in the atendimentos

--- socket-io/io.js
+++ socket-io/io.js
@@ -1,23 +1,31 @@
 const socketIO = require('socket.io');
-const { isNil } = require('ramda');
+const { isNil, both } = require('ramda');
 
 let io = null;
+let connectedTecnicos = [];
+
 module.exports = ( server ) => {
 
     if(isNil(io)){
         io = new socketIO(server);
-    } 
+    }else if(both(isNil(server), isNil(io))){
+        throw new Error("Socket io not initilized!!!");
+    }
 
     io.on('connection', (socket) => {
 
         console.log("novo socket!!!");
-        require("./registerTecnico")(socket);
+        const registerTecnico = require("./registerTecnico")(socket);
+        connectedTecnicos = registerTecnico.getTecnicos();
         
     })
 
     return {
-        enviarUpdateTecnico : () => {
-            io.sockets.forEach(s => console.log(s));
+        enviarUpdateTecnico : (nome) => {
+            const socket = connectedTecnicos.find(s => s.tecnico.name = nome);
+            if(socket){
+                socket.imit("updateAtendimentos");
+            }
         }
     }
 
--- socket-io/registerTecnico.js
+++ socket-io/registerTecnico.js
@@ -1,14 +1,28 @@
+let connectedTenicos = [];
+
 const registerTecnico = (socket) => {
 
     let addedUser = false;
 
     socket.on("registra-tecnico", (dadosTecnico) => {
-        console.log("novo tecnico: ");
-        console.log(dadosTecnico);
+
         if(!addedUser){
             socket.tecnico = dadosTecnico;
         }
+        connectedTenicos.push(socket);
+    });
+
+    socket.on("disconnect", () =>{
+        console.log("Desconecting")
+        console.log(socket.tecnico);
+        connectedTenicos = connectedTenicos.filter( s => s !== socket);
     })
+
+    return {
+        getTecnicos: () => {
+            return connectedTenicos;
+        }
+    }
 }
 
 module.exports = registerTecnico;
