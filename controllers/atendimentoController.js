c953c47493b4415b02489039e199236ebefdeeb7
alexandrejuk <ale_santos.soares@hotmail.com>
2017-11-27 14:20:41 -0200

    add: routes with query of associado and data, add roles of access
    created routes with query of associado and data, add roles of
    access

--- controllers/atendimentoController.js
+++ controllers/atendimentoController.js
@@ -5,141 +5,143 @@ const Promise = require('bluebird');
 const axios = require('axios');
 const formatAtendimento = require('../utils/atendimentoSpec');
 
-
-const getAll = ( req, res, next ) => {
-    
+const getAll = (req, res, next) => {
+  if (req.query.associado && req.query.data) {
+    Atendimentos.find({
+      data_atendimento: { $eq: req.query.data },
+      "tecnico.nome": { $exists: true, $ne: "" }
+    })
+      .then(atendimentos => {
+        res.json(atendimentos);
+      })
+      .catch(error => next(error));
+  } else if (req.query.associado) {
+    Atendimentos.find({ "tecnico.nome": { $exists: true, $ne: "" } })
+      .then(atendimentos => {
+        res.json(atendimentos);
+      })
+      .catch(error => next(error));
+  } else {
     Atendimentos.find(req.query)
-    .then( atendimentos => {
-        //const _io = io;
-        io.enviarUpdateTecnico(321321321);
-        res.json( atendimentos ) 
-    })
-    .catch( error => next(error) )
+      .then(atendimentos => {
+        res.json(atendimentos);
+      })
+      .catch(error => next(error));
+  }
+};
 
-}
+const atendimentoNew = (req, res, next) => {
+  const atendimento = formatAtendimento(req.body);
+  const atendimentoModel = new Atendimentos(atendimento);
 
-const atendimentoNew = ( req, res, next ) => {
+  atendimentoModel
+    .save()
+    .then(savedAtendimento => res.json(savedAtendimento))
+    .catch(error => next(error));
+};
 
-    const atendimento = formatAtendimento(req.body);
-    const atendimentoModel = new Atendimentos(atendimento);
+const updateAtendimento = (req, res, next) => {
+  const atendimento = formatAtendimento(req.body);
+  const _id = prop('id', req.params);
 
-    atendimentoModel.save()
-    .then( savedAtendimento => res.json(savedAtendimento) )
-    .catch( error => next(error) )
-    
-}
-
-const updateAtendimento = ( req, res, next ) => {
-
-    const atendimento = formatAtendimento(req.body);
-    const _id = prop("id", req.params);
-
-    Atendimentos.findByIdAndUpdate(_id, atendimento, {
-        runValidators: true
-    })
+  Atendimentos.findByIdAndUpdate(_id, atendimento, {
+    runValidators: true
+  })
     .then(updatedAtendimento => updatedAtendimento._id)
     .then(id => Atendimentos.findById(id))
-    .then( updatedData => res.json(updatedData) )
-    .catch( error => next(error) )
-}
+    .then(updatedData => res.json(updatedData))
+    .catch(error => next(error));
+};
 
-const getAtendimentoByID = ( req, res, next ) => {
+const getAtendimentoByID = (req, res, next) => {
+  const _id = prop('id', req.params);
+  Atendimentos.findById(_id)
+    .then(atendimento => res.json(atendimento))
+    .catch(error => next(error));
+};
 
-    const _id = prop("id", req.params);
-    Atendimentos.findById(_id)
-    .then(atendimento => res.json(atendimento) )
-    .catch( error => next(error) )
-    
-}
+const patchAtendimentos = (req, res, next) => {
+  const atendimentosData = prop('body', req);
 
+  if (Array.isArray(atendimentosData)) {
+    const atendimentosPromise = atendimentosData.map(atendimentoData => {
+      return Atendimentos.findByIdAndUpdate(
+        atendimentoData._id,
+        atendimentoData
+      );
+    });
 
-const patchAtendimentos = ( req, res, next ) => {
+    Promise.all(atendimentosPromise)
+      .then(atendimento => res.json(atendimento))
+      .catch(error => next(error));
+  } else {
+    next(new Error('You have not passed an array'));
+  }
+};
 
-    const atendimentosData = prop("body", req);
+const getTodosAtendimentosDosEmpregados = (req, res, next) => {
+  const data = req.query.data || new Date();
 
-    if(Array.isArray(atendimentosData)){
+  const login = {
+    username: 'vlima',
+    password: 'redhat'
+  };
 
-        const atendimentosPromise = atendimentosData.map(atendimentoData =>{
-             return Atendimentos.findByIdAndUpdate(atendimentoData._id, atendimentoData)
-        })
-        
-        Promise.all(atendimentosPromise)
-        .then(atendimento => res.json(atendimento) )
-        .catch( error => next(error) )
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  };
 
-    }else{
-        next(new Error("You have not passed an array"));
-    }
-    
-}
-
-
-const getTodosAtendimentosDosEmpregados = ( req, res, next ) => {
-
-    const data  = req.query.data || new Date() ;
-
-    const login = {
-        username: 'vlima',
-        password: 'redhat'
-    };
-
-    const headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    };
-
-    axios.request({
+  axios
+    .request({
       headers,
       data: JSON.stringify(login),
       url: 'http://165.227.78.113:3000/login',
       method: 'POST'
     })
-    .then( result => result.data.token)
-    .then( token  => {
-        return Object.assign({}, headers, { 'Authorization': 'Bearer ' + token })
+    .then(result => result.data.token)
+    .then(token => {
+      return Object.assign({}, headers, { Authorization: 'Bearer ' + token });
     })
-    .then( headers => ({
+    .then(headers => ({
       headers,
       url: 'http://165.227.78.113:3000/api/funcionarios',
       method: 'GET'
     }))
-    .then( axios.request )
-    .then( result => result.data)
-    .then(  funcionarios => {
-        
-        getAtendimentosPorData(data).then(atendimentos => {
+    .then(axios.request)
+    .then(result => result.data)
+    .then(funcionarios => {
+      getAtendimentosPorData(data).then(atendimentos => {
+        const funcAtendimentos = funcionarios.map(funcionario => {
+          const atendimentosFunc = atendimentos.filter(
+            atendimento => atendimento.tecnico._id === funcionario._id
+          );
+          funcionario.atendimentos = atendimentosFunc;
+          return funcionario;
+        });
 
-            const funcAtendimentos = funcionarios.map( funcionario => {
-                const atendimentosFunc = atendimentos.filter(atendimento => atendimento.tecnico._id === funcionario._id )
-                funcionario.atendimentos = atendimentosFunc;
-                return funcionario;
-            });
-
-            res.json(funcAtendimentos);
-
-        })
+        res.json(funcAtendimentos);
+      });
     })
-    .catch( error => next(error) )
-    
-}
+    .catch(error => next(error));
+};
 
-const getAtendimentosPorData = ( data ) => {
+const getAtendimentosPorData = data => {
+  const dataObj = new Date(data);
 
-    const dataObj = new Date(data);
-
-    return Atendimentos.find({})
+  return Atendimentos.find({})
     .where('data_atendimento')
     .gte(new Date(dataObj.getFullYear(), dataObj.getMonth(), dataObj.getDate()))
-    .lt(new Date(dataObj.getFullYear(), dataObj.getMonth(), dataObj.getDate() + 1 ))
-
-}
-
+    .lt(
+      new Date(dataObj.getFullYear(), dataObj.getMonth(), dataObj.getDate() + 1)
+    );
+};
 
 module.exports = {
-    getAll,
-    patchAtendimentos,
-    atendimentoNew,
-    updateAtendimento,
-    getAtendimentoByID,
-    getTodosAtendimentosDosEmpregados
-}
+  getAll,
+  patchAtendimentos,
+  atendimentoNew,
+  updateAtendimento,
+  getAtendimentoByID,
+  getTodosAtendimentosDosEmpregados
+};
--- routes/atendimentoRoute.js
+++ routes/atendimentoRoute.js
@@ -2,14 +2,15 @@ const route = require("express").Router();
 const atendimentoController = require('../controllers/atendimentoController');
 const uploadController = require('../controllers/uploadController');
 const express = require('express');
+const rolesMiddleware = require('../middleware/roleMiddleware');
 
-route.get('/atendimentos', atendimentoController.getAll);
-route.patch('/atendimentos', atendimentoController.patchAtendimentos);
-route.get('/atendimentos/:id', atendimentoController.getAtendimentoByID);
-route.post('/atendimentos', atendimentoController.atendimentoNew);
-route.put('/atendimentos/:id', atendimentoController.updateAtendimento);
-route.get('/funcionariosatendimentos/', atendimentoController.getTodosAtendimentosDosEmpregados);
-route.post('/atendimentos/:id/imagens', uploadController.uploadingHandler, uploadController.atendimentoUpload)
+route.get('/atendimentos', rolesMiddleware(['all']), atendimentoController.getAll);
+route.patch('/atendimentos', rolesMiddleware(['all']), atendimentoController.patchAtendimentos);
+route.get('/atendimentos/:id', rolesMiddleware(['all']), atendimentoController.getAtendimentoByID);
+route.post('/atendimentos', rolesMiddleware(['all']), atendimentoController.atendimentoNew);
+route.put('/atendimentos/:id', rolesMiddleware(['all']), atendimentoController.updateAtendimento);
+route.get('/funcionariosatendimentos/', rolesMiddleware(['all']), atendimentoController.getTodosAtendimentosDosEmpregados);
+route.post('/atendimentos/:id/imagens', rolesMiddleware(['all']), uploadController.uploadingHandler, uploadController.atendimentoUpload)
 
 //route.put('/atendimentos/funcionario/:id', atendimentoController.getUPFromUddnwind);
 
