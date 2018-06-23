44c85256d1f2d1ef627ac50a7f06e88b59b43749
vitorsilvalima <vitorsilvalima29@gmail.com>
2018-06-23 01:16:40 -0300

    scripts: add scripts to easily generate atendimentos

--- config/googleMapsAPIConfig.js
+++ config/googleMapsAPIConfig.js
@@ -2,13 +2,13 @@ const { getConfig } = require('./utils');
 
 const config = {
   development: {
-    apiKey: null,
+    apiKey: process.env.GOOGLE_MAPS_API_KEY || null,
   },
   test: {
-    apiKey: null,
+    apiKey: process.env.GOOGLE_MAPS_API_KEY|| null,
   },
   production: {
-    apiKey: null,
+    apiKey: process.env.GOOGLE_MAPS_API_KEY || null,
   }
 };
 
---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+const antedimento = require('./antedimento');
+
+module.exports = {
+  antedimento,
+}
